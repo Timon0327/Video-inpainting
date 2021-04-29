448e3bd18e3406febe009f54adef767cac46b33a
Timon0327 <78549850+Timon0327@users.noreply.github.com>
2021-04-29 17:02:02 +0800

    Update build_graph.py

--- build_graph.py
+++ build_graph.py
@@ -5,6 +5,13 @@ import math
 import scipy.sparse as sparse
 from torch.nn.parameter import Parameter
 
+'''
+
+The function of build graph, but it is random to define the node, 
+when the features are got, it will be replaced.
+
+'''
+
 nodes = []
 node_trans = []
 
@@ -54,17 +61,25 @@ adj_T = upper_X + upper_X.T - sparse.diags(adj.diagonal())
 # # print(adj_T)
 # print(adj_T.sum(1))
 
-
+# method 1
+# def normalize(mx):
+#     rowsum = np.array(mx.sum(1))  # degree D
+#     r_inv = np.power(rowsum, -1).flatten() # norm，A^=(D~)^-1 A~
+#     r_inv[np.isinf(r_inv)] = 0.
+#     r_mat_inv = sp.diags(r_inv)
+#     mx = r_mat_inv.dot(mx)
+#     return mx
+# method 2
 def normalize(mx):
-    rowsum = np.array(mx.sum(1))  # degree D
-    r_inv = np.power(rowsum, -1).flatten()
-    r_inv[np.isinf(r_inv)] = 0.
-    r_mat_inv = sp.diags(r_inv)
-    mx = r_mat_inv.dot(mx)
+    degree = np.array(mx.sum(1))  # degree D
+    degree = sp.diags(np.power(degree, -1).flatten()) # norm，A^=(D~)^-1 A~
+    mx = degree.dot(mx)
     return mx
 
+#A^=I+A
 adj = normalize(adj_T + sp.eye(adj_T.shape[0]))
 # print(adj)
+
 
 
 
