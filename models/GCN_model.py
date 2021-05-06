a2a3eac7eb4f9f49e9c9bf732b501047a393cdae
Timon0327 <78549850+Timon0327@users.noreply.github.com>
2021-05-06 21:40:54 +0800

    Add files via upload

---
+++ models/GCN_model.py
@@ -0,0 +1,101 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import build_graph
+import scipy.sparse as sp
+import numpy as np
+
+
+class GCN_spatial(nn.Module):
+    '''
+
+    Z = AXW
+
+    '''
+
+    def __init__(self, adj, feature_dim, out_dim):
+        super(GCN_spatial, self).__init__()
+        self.A = adj
+        # self.X = X
+        self.fc1 = nn.Linear(feature_dim, feature_dim, bias=False)
+        self.fc2 = nn.Linear(feature_dim, feature_dim, bias=False)
+        self.fc3 = nn.Linear(feature_dim, out_dim, bias=False)
+
+    def forward(self, X):
+        X = F.leaky_relu(self.fc1(adj.mm(X)))
+        X = F.leaky_relu(self.fc2(adj.mm(X)))
+        X = self.fc3(adj.mm(X))
+        maxpooling = torch.nn.MaxPool2d(2, stride=2)
+        X = maxpooling(X)
+        # print(X)
+        return X
+
+
+class GCN_temporal(nn.Module):
+    '''
+
+    Z = AXW
+
+    '''
+
+    def __init__(self, adj, feature_dim, out_dim):
+        super(GCN_temporal, self).__init__()
+        self.A = adj
+        # self.X = X
+        self.fc1 = nn.Linear(feature_dim, feature_dim, bias=False)
+        self.fc2 = nn.Linear(feature_dim, feature_dim, bias=False)
+        self.fc3 = nn.Linear(feature_dim, out_dim, bias=False)
+
+    def forward(self, X):
+        X = F.leaky_relu(self.fc1(adj.mm(X)))
+        X = F.leaky_relu(self.fc2(adj.mm(X)))
+        X = self.fc3(adj.mm(X))
+        # print('x', X)
+        maxpooling = torch.nn.MaxPool2d(2, stride=2)
+        output = maxpooling(X)
+        # print(X)
+        return output
+
+
+class spatial(nn.Module):
+    '''
+
+    Z = AXW
+
+    '''
+
+    def __init__(self, adj, feature_dim, out_dim):
+        super(GCN_spatial, self).__init__()
+        self.A = adj
+        # self.X = X
+        self.fc1 = nn.Linear(feature_dim, feature_dim, bias=False)
+        self.fc2 = nn.Linear(feature_dim, feature_dim, bias=False)
+        self.fc3 = nn.Linear(feature_dim, out_dim, bias=False)
+
+    def forward(self, X):
+        X = F.leaky_relu(self.fc1(adj.mm(X)))
+        X = F.leaky_relu(self.fc2(adj.mm(X)))
+        X = self.fc3(adj.mm(X))
+        # print('x', X)
+        maxpooling = torch.nn.MaxPool2d(2, stride=2)
+        output = maxpooling(X)
+        # print(X)
+        return output
+
+
+dataset = build_graph.nodes
+new_data = torch.zeros(len(dataset), dataset[0].shape[1], dtype=torch.float32)
+for i in range(len(dataset)):
+    new_data[i] = dataset[i]
+
+# print(new_data)
+
+feature_dim = 3
+# print(feature_dim)
+out_dim = 2048
+adj = build_graph.adj  # print(adj)
+# print(adj)
+model_temporal = GCN_temporal(adj_t, feature_dim, out_dim)
+model_sptial = GCN_spatial(adj_s, feature_dim, out_dim)
+# feature_spa = model(new_data)
+# a = adj.mm(new_data)
