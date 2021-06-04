238d45cc4330de5186f7937463e9cd96b9352c82
captaincj <captain0848@outlook.com>
2021-06-04 15:24:44 +0800

    bugs

--- models/GCN_model.py
+++ models/GCN_model.py
@@ -144,11 +144,12 @@ class GCN(nn.Module):
         out = torch.transpose(out, dim0=1, dim1=2)  # [N, 2048, nodes]
         # batch = out.size()[0]
         out = out.reshape(-1, 2 * self.frames, self.slice, self.slice)     # [N * 2048, 2 * nodes, slice, slice]
+        print(out.size())
 
         out_conv1 = self.conv1x1(out)     # [N * 2048, 1, slice, slice]
         out = torch.squeeze(out_conv1)    # [N * 2048, slice, slice]
         print('GCN batch ', self.batch)
-        print(out.size())
+
         out = out.view(self.batch, 2048, self.slice, self.slice)
 
         # for the case where slice is 2 and height of img is 640
