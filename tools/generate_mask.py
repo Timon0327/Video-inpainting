917986244911356b80211e2621271f48e6b9975b
captaincj <captain0848@outlook.com>
2021-06-06 00:03:28 +0800

    adjust path

--- tools/generate_mask.py
+++ tools/generate_mask.py
@@ -6,6 +6,8 @@ import numpy as np
 import cv2 as cv
 import os
 from utils.image import rand_bbox_mask
+import sys
+sys.path.append('..')
 from cfgs import config
 
 data_dir = '/mnt/qinlikun/dataset/DAVIS/DAVIS-semisupervised/DAVIS-trainval/JPEGImages/480p'
