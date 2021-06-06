f4444795103d5d1240f28e8ca8bd75ca138f1a1f
captaincj <captain0848@outlook.com>
2021-06-06 19:47:12 +0800

    fix bug in load model

---
+++ tools/fix_model.py
@@ -0,0 +1,34 @@
+'''
+#  filename: fix_model.py
+#  fix keys in checkpoint
+#  By Likun Qin, 2021
+'''
+import torch
+import copy
+import os
+
+model_dir = '/mnt/qinlikun/inpainting/ckpt'
+
+
+def fix(model):
+    '''
+    @param model: model path
+    '''
+    ckpt = torch.load(model)
+    state_dict = ckpt['flownetcg']
+    result = copy.deepcopy(state_dict)
+    for key in list(state_dict):
+        values = state_dict[key]
+        new_key = key[7:]
+        del result[key]
+        result[new_key] = values
+
+    print(model, ' done!')
+    ckpt['flownetcg'] = result
+    filename = model.split('.')[0] + 'new.pt'
+    torch.save(ckpt, os.path.join(model_dir,filename))
+
+if __name__ == '__main__':
+    models = os.listdir(model_dir)
+    for one in models:
+        fix(one)
