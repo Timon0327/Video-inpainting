b86f9025a24da361dc54900a04c2df0c381e9656
captaincj <captain0848@outlook.com>
2021-06-19 18:08:53 +0800

    change path

--- tools/update_names.py
+++ tools/update_names.py
@@ -3,9 +3,9 @@ import sys
 sys.path.append('..')
 import cfgs.config_ytb as config
 
-for j in range(2, 42):
+for j in range(1, 9):
 
-    test_dir = os.path.join('/home/cap/dataset/demo_ytb/', str(j), 'frames')
+    test_dir = os.path.join('/mnt/qinlikun/dataset/test/ytb', str(j), 'frames')
 
     f_list = os.listdir(test_dir)
     for one in f_list:
@@ -20,30 +20,30 @@ for j in range(2, 42):
         os.rename(os.path.join(test_dir, one), os.path.join(test_dir, '%05d.jpg' % (i)))
 
 
-    test_dir = os.path.join('/home/cap/dataset/demo_ytb/', str(j), 'flow')
-
-    f_list = os.listdir(test_dir)
-    for one in f_list:
-        name = one.split('.')[0]
-        os.rename(os.path.join(test_dir, one), os.path.join(test_dir, '0' + one))
-
-    f_list = os.listdir(test_dir)
-    flos = []
-    rflows = []
-    for one in f_list:
-        if 'rflo' in one:
-            rflows.append(one)
-        else:
-            flos.append(one)
-    print(flos)
-    flos.sort()
-    rflows.sort()
-    for i, one in enumerate(flos):
-        name = one.split('.')[0]
-        affix = one.split('.')[1]
-        os.rename(os.path.join(test_dir, one), os.path.join(test_dir, '%05d.' % (i) + affix))
-
-    for i, one in enumerate(rflows):
-        name = one.split('.')[0]
-        affix = one.split('.')[1]
-        os.rename(os.path.join(test_dir, one), os.path.join(test_dir, '%05d.' % (i+1) + affix))
+    # test_dir = os.path.join('/home/cap/dataset/demo_ytb/', str(j), 'flow')
+    #
+    # f_list = os.listdir(test_dir)
+    # for one in f_list:
+    #     name = one.split('.')[0]
+    #     os.rename(os.path.join(test_dir, one), os.path.join(test_dir, '0' + one))
+    #
+    # f_list = os.listdir(test_dir)
+    # flos = []
+    # rflows = []
+    # for one in f_list:
+    #     if 'rflo' in one:
+    #         rflows.append(one)
+    #     else:
+    #         flos.append(one)
+    # print(flos)
+    # flos.sort()
+    # rflows.sort()
+    # for i, one in enumerate(flos):
+    #     name = one.split('.')[0]
+    #     affix = one.split('.')[1]
+    #     os.rename(os.path.join(test_dir, one), os.path.join(test_dir, '%05d.' % (i) + affix))
+    #
+    # for i, one in enumerate(rflows):
+    #     name = one.split('.')[0]
+    #     affix = one.split('.')[1]
+    #     os.rename(os.path.join(test_dir, one), os.path.join(test_dir, '%05d.' % (i+1) + affix))
